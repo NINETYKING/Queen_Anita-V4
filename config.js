e03febcb6b4e83e8dbdade769cfc5a9b84e51b29
David Cyril <victim0mitciv@gmail.com>
2025-02-26 23:53:54 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config(); 
 
-global.owner = process.env.OWNER_NUMBER.split(','); 
+global.owner = process.env.OWNER_NUMBER; 
 global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
 global.ownername = process.env.OWNER_NAME || "David Cyril";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
@@ -28,7 +28,6 @@ global.roles = {
     25: "Legend",
 };
 
-global.sudoUsers = process.env.SUDO_USERS.split(',').map(user => `${user}@s.whatsapp.net`); 
 
 global.public = process.env.PUBLIC === 'true';
 
